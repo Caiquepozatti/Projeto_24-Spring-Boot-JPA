70f6c20af48e8c926df237aee8d21814864725d1
Caique Pozatti <caca_pozatti@hotmail.com>
2023-07-18 14:22:47 -0300

    Project created

---
+++ src/main/java/com/pozatticaique/projeto24/Projeto24Application.java
@@ -0,0 +1,13 @@
+package com.pozatticaique.projeto24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pozatticaique/projeto24/Projeto24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pozatticaique.projeto24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

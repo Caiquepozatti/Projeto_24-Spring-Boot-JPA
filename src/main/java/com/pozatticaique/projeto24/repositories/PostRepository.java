d38c6f3797da72dfdea8c5cddddca5b498d1207c
Caique Pozatti <caca_pozatti@hotmail.com>
2023-07-21 11:50:07 -0300

    Simple consult with @Query

--- src/main/java/com/pozatticaique/projeto24/repositories/PostRepository.java
+++ src/main/java/com/pozatticaique/projeto24/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.pozatticaique.projeto24.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pozatticaique.projeto24.entities.Post;
@@ -14,4 +15,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
 	//https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 	List<Post> findByTitleContainingIgnoreCase(String text); //Igonorar letras maiusculas
+	
+	//Consulta com @Query
+	//https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{'title': { $regex: ?0, $options: 'i'} }")//Pegar como parametro a String text (?0), (Options = i) == IgonreCase
+	List<Post> searchTitle(String text);
 }
